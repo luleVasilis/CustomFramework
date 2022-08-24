7cc19894d3e2816ba6a8636df7704c8e63e93a0e
Vasilis Lule <lule@sycada.com>
2022-08-24 12:13:07 +0300

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,17 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Eldison Lule on 24/8/22.
+//
+
+import Foundation
+
+ public struct SDK {
+     
+     
+     public static func doSomeWork() {
+         print(" Helloo Work")
+     }
+     
+}
